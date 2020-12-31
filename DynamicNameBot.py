57a495f20028b92d43cb093cd2babbbc8078739c
Matt <mcmader1@gmail.com>
2020-12-31 00:29:50 -0700

    Big Update
    Bot should better handle 2 users in chat now, prioritizing the game over just chatting
    This update puts more emphasis on games. If 2 users are just chatting, 1 is playing league and one is playing minecraft. It will choose league or minecraft as the name. Even though more members are just chatting, I personally want it to focus hard on the gaming aspect of the users

--- DynamicNameBot.py
+++ DynamicNameBot.py
@@ -28,21 +28,27 @@ class MyClient(discord.Client):
 
     async def check_game_update(self):
         await self.wait_until_ready()
-        activityList = []
         while not self.is_closed():
             for guild in self.guilds:
                 for channel in guild.voice_channels:
                     if(len(channel.members) > 0):
+                        numChatting = 0
+                        activityList = []
                         for chatter in channel.members:
-                            if(chatter.activity != None and chatter.activity.type.name == 'playing'):
+                            if(chatter.activity != None):
+                                numChatting += 0
+                            elif(chatter.activity.type.name == 'playing' or chatter.activity.type.name == 'streaming'):
                                 activityList.append(chatter.activity.name)
                             else:
-                                activityList.append('Just Chatting')
-                        new_name = str(Counter(activityList).most_common(1)[0][0])
-                        await channel.edit(name=new_name) #Can only send 2 changes per 10 minutes? So do it every 5 I think
+                                numChatting += 0 
+
+                        if(numChatting > (len(channel.members)/2)):
+                            new_name = 'Just Chatting'
+                        else:
+                            new_name = str(Counter(activityList).most_common(1)[0][0])
+                        await channel.edit(name=new_name)
 #                        channel2 = self.get_channel(791066555085094923)
 #                        await channel2.send(new_name)
-                        activityList.clear()
                 await asyncio.sleep(300) # task runs every 5 Minutes, temp set to 5 seconds when sending messages instead of updating channel name
 
 
